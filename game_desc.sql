723ec458086929ee6be4e11fac6d5ec522a4be3e
Guilherme Henrique Galdini Tosi <galdiniguilherme@usp.br>
2022-11-03 06:33:21 -0300

    Update game_desc.sql

--- game_desc.sql
+++ game_desc.sql
@@ -11,7 +11,10 @@ INSERT
         (8, 'Anime'),
         (9, 'Puzzle'),
         (10, 'FPS'),
-        (11, 'Shooter');
+        (11, 'Shooter'),
+        (12, 'Point-And-Click'),
+        (13, 'MOBA'),
+        (14, 'Race');
 
 INSERT
 	INTO games_productors
@@ -28,22 +31,25 @@ INSERT
         (10, 'SNK', 'Japan'),
         (11, 'Pipeworks Studio', 'EUA'),
         (12, 'Gorka Sutdios', 'EUA'),
-        (13, 'Nintendo', 'Japan');
+        (13, 'Nintendo', 'Japan'),
+        (14, 'Tell Tale', 'EUA'),
+        (15, 'Riot Games', 'EUA');
         
 INSERT
 	INTO games_list
 	VALUES
-		(1, 'The Elder Scrolls IV: Oblivion', 1, 7, 8.1, 'Description: RPG game in first person mode, based in complete tasks to advance in the main history.', true),
-        (2, 'Street Fighter', 2, 4, 9.2, 'Description: Historic Fight Game where you can beat and launch power by doing AMAZING combos', true),
-        (3, 'Mega Man', 3, 8, 8.3, 'Description: Indie game by sega where you control Mega man, fighting enemies, collecting improvements for himself', true),
-        (4, 'Fifa 15', 4, 2, 7.7, 'Description: Soccer game, with AMAZING graphics and totally updated', false),
-        (5, 'Bomberman', 13, 1, 8.1, 'Description: Platform nd aventure game based in defeat your enemies by plating bombs', false),
-        (6, 'Metal Slug', 10, 3, 9.1, 'Description: Shoot game in third person mode and multiplayer, you defeat enemies and advance in map trying to conclude the mission lauching bombs and giving bullets and more bullets', true),
-        (7, 'Cat Mario', 12, 6, 4.7, 'Description: Indie game, that can do you get angry anytime', false),
-        (8, 'Terraria', 11, 6, 6.9, 'Description: Indie game based in exploration, improve elements, kill enemies, complete missions. Its 3rd person mode and multiplayer (local and server)', false),
-        (9, 'Counter Strike: Global Offensive', 9, 11, 8.8, 'Description: FPS globally famous, 5x5, buying guns and killing your oponents to get the victory', true);
-        
-        INSERT
+	(1, 'The Elder Scrolls IV: Oblivion', 1, 7, 8.1, 'Description: RPG game in first person mode, based in complete tasks to advance in the main history.', true, 'oblivion.png'),
+        (2, 'Street Fighter', 2, 4, 9.2, 'Description: Historic Fight Game where you can beat and launch power by doing AMAZING combos', true, 'streetFighter.png'),
+        (3, 'Mega Man', 3, 8, 8.3, 'Description: Indie game by sega where you control Mega man, fighting enemies, collecting improvements for himself', true, 'Megaman.png'),
+        (4, 'Fifa 15', 4, 2, 7.7, 'Description: Soccer game, with AMAZING graphics and totally updated', false, 'FIFA.png'),
+        (5, 'Bomberman', 13, 1, 8.1, 'Description: Platform nd aventure game based in defeat your enemies by plating bombs', false, 'Bomberman.png'),
+        (6, 'Metal Slug', 10, 3, 9.1, 'Description: Shoot game in third person mode and multiplayer, you defeat enemies and advance in map trying to conclude the mission lauching bombs and giving bullets and more bullets', true, 'Metalslug.png'),
+        (7, 'Cat Mario', 12, 6, 4.7, 'Description: Indie game, that can do you get angry anytime', false, 'Catmario.png'),
+        (8, 'Terraria', 11, 6, 6.9, 'Description: Indie game based in exploration, improve elements, kill enemies, complete missions. Its 3rd person mode and multiplayer (local and server)', false, 'Terraria.png'),
+        (9, 'Counter Strike: Global Offensive', 9, 11, 8.8, 'Description: FPS globally famous, 5x5, buying guns and killing your oponents to get the victory', true, 'CSGO.png'),
+        (10, "League of Legends", 15, 13, 8.6, "Description: most famous MOBA game, walk  Summoners Rift, killing your enemies, doing the objectives to win the game", false, "lol.png");
+      
+      INSERT
 			INTO users
 			VALUES
 				(1, 'Guilherme', 21, 'Male', 'Tosi.eu', 'f09ac54b1005a15ac70bcf5fd10d8987'),
